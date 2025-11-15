6d2d36ce2f22c8e8e1e6f93f34423cd78140294e
AmiiGood <alexisalvarez1234563@outlook.com>
2025-11-14 20:06:20 -0600

    add task

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addTask } from "../features/tasks/taskSlice";
+import { v4 as uuid } from "uuid";
 
 const TaskForm = () => {
-  return <div>TaskForm</div>;
+  //Agregamos useState para guardar los cambios del formulario
+  const [task, setTask] = useState({
+    title: "",
+    description: "",
+  });
+
+  //Agregamos acceso al dispatch de Redux
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setTask({
+      ...task,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(
+      addTask({
+        ...task,
+        id: uuid(),
+      })
+    );
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="title"
+        placeholder="Title"
+        onChange={handleChange}
+      />
+      <textarea
+        name="description"
+        placeholder="description"
+        onChange={handleChange}
+      />
+      <button>Save</button>
+    </form>
+  );
 };
 
 export default TaskForm;
